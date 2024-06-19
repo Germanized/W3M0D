5d402544d7dd6726decec495b6c885858a8c6aa4
Germanized <91925088+Germanized@users.noreply.github.com>
2024-06-19 13:32:35 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('download-btn').addEventListener('click', function() {
+    var disclaimer = document.getElementById('disclaimer');
+    disclaimer.classList.remove('hidden');
+    setTimeout(function() {
+        window.location.href = 'https://mega.nz/folder/MmoT3QpY#Uh6a7oTwOKZ2uawZU8kxSg';
+    }, 3000);
+});
