5200e2d15fe9cec10319a4bb0c454d6d3916e3e9
Germanized <91925088+Germanized@users.noreply.github.com>
2024-06-19 15:21:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ document.getElementById('download-btn').addEventListener('click', function() {
         window.location.href = 'https://mega.nz/folder/MmoT3QpY#Uh6a7oTwOKZ2uawZU8kxSg';
     }, 3000);
 });
+
+document.getElementById('discord-btn').addEventListener('click', function() {
+    window.location.href = 'https://discord.gg/yrr3QERx9d';
+});
