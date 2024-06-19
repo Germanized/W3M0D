64efe3ad31d63bb9dae32fd2de5d89e5fdcf04c9
Germanized <91925088+Germanized@users.noreply.github.com>
2024-06-19 13:49:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.getElementById('download-btn').addEventListener('click', function() {
         window.location.href = 'https://mega.nz/folder/MmoT3QpY#Uh6a7oTwOKZ2uawZU8kxSg';
     }, 3000);
 });
+
+document.addEventListener('mousemove', function(e) {
+    var body = document.querySelector('body');
+    var x = (e.clientX / window.innerWidth) - 0.5;
+    var y = (e.clientY / window.innerHeight) - 0.5;
+    body.style.transform = 'translate(' + x * 10 + 'px, ' + y * 10 + 'px)';
+});
