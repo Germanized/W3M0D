fb87ddbfdec05b49dca16e16ab4195de615b98ec
Germanized <91925088+Germanized@users.noreply.github.com>
2024-06-19 15:03:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ document.getElementById('download-btn').addEventListener('click', function() {
 });
 
 document.addEventListener('mousemove', function(e) {
+    // Exclude body transformation if disclaimer is shown
+    var disclaimer = document.getElementById('disclaimer');
+    if (!disclaimer.classList.contains('hidden')) {
+        return;
+    }
+
     var body = document.querySelector('body');
     var x = (e.clientX / window.innerWidth) - 0.5;
     var y = (e.clientY / window.innerHeight) - 0.5;
     body.style.transform = 'translate(' + x * 10 + 'px, ' + y * 10 + 'px)';
 });
-/*hi*/
