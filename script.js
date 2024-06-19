0665c18428e9ba0c9bff267f53fdccb129d11d94
Germanized <91925088+Germanized@users.noreply.github.com>
2024-06-19 15:12:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,3 @@ document.getElementById('download-btn').addEventListener('click', function() {
         window.location.href = 'https://mega.nz/folder/MmoT3QpY#Uh6a7oTwOKZ2uawZU8kxSg';
     }, 3000);
 });
-
-// Optionally, remove the following mousemove event listener or modify it not to affect body transformation
-/*
-document.addEventListener('mousemove', function(e) {
-    var disclaimer = document.getElementById('disclaimer');
-    if (!disclaimer.classList.contains('hidden')) {
-        return;
-    }
-
-    var body = document.querySelector('body');
-    var x = (e.clientX / window.innerWidth) - 0.5;
-    var y = (e.clientY / window.innerHeight) - 0.5;
-    body.style.transform = 'translate(' + x * 10 + 'px, ' + y * 10 + 'px)';
-});
-*/
